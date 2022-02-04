9121e8b9866975c0b9a8f88eb65c0c24f93d8f21
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2022-02-04 21:04:15 +0530

    b

--- ak.py
+++ ak.py
@@ -1,11 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-f=open("s.html")
-str=f.read()
-print(str)
 @app.route('/')
 def hello_world():
-   return  str
+   return  "hello world "
 
 if __name__ == '__main__':
    app.run()
